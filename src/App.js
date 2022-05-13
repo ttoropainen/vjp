4d137cf3f6fc6b7c8aad0204515230c999033022
Janne Silfverhuth <silfvej@aalto.fi>
2022-05-13 16:30:22 +0300

    lisättty navbar ja sectionit ja lorem ipmsumia blogeihin

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit CCXDFYUIUYGFGYUYGFUGUG <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div id="container">
+        <section id="navigation">
+                <nav>
+                <ul>
+                    <li><a>koti</a></li>
+                    <li><a>quiz</a></li>
+                    <li><a>info</a></li> 
+                    <li><a>blogit</a></li>
+                    <li><a>some</a></li>
+                </ul>
+                </nav>
+        </section>
+        <section id="koti">
+        </section>
+        <section id="quiz">
+        </section>
+        <section id="info">
+        </section>
+        <section id="blogit">
+          <div> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper nibh eu luctus mattis. Donec tincidunt sollicitudin enim rutrum vulputate. Phasellus quam elit, accumsan nec est et, posuere tempus odio. In ac felis aliquam, bibendum orci non, dapibus augue. Pellentesque bibendum consectetur nisi, ac porta elit tincidunt in. Aliquam nunc massa, eleifend non tortor sit amet, pellentesque bibendum augue. Maecenas vitae sollicitudin ipsum. Pellentesque arcu justo, molestie at justo sit amet, luctus pellentesque ligula.
+                Nullam tincidunt commodo augue, pulvinar laoreet mauris sollicitudin maximus. Nam pellentesque vestibulum arcu, quis aliquet turpis malesuada ut. Praesent vel aliquet lectus. Fusce eu odio nec ex convallis sodales. Nunc luctus, erat non laoreet congue, mi quam elementum risus, sit amet rutrum metus elit aliquet leo. Sed scelerisque vitae elit congue ullamcorper. Nam iaculis urna lectus, et posuere mi maximus eu. Suspendisse nec metus eu ex venenatis dapibus
+          </div>
+        </section>
+        <section id="some">
+        </section>
+        <section id="slogan">
+        </section>
+      </div>
       </header>
     </div>
   );
