e5d9cbe6ce61f571f3ebc110f45a1cd5318a095d
Tuomas Toropainen <tuomastoropainen@Tuomass-MacBook-Pro.local>
2022-05-16 16:16:30 +0300

    länderi, video ja fixed kioski logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 
-
 function App() {
   return (
     <div className="App">
@@ -9,19 +8,33 @@ function App() {
         <section id="navigation">
                 <nav>
                 <ul>
-                    <li><a>koti</a></li>
-                    <li><a>quiz</a></li>
-                    <li><a>info</a></li> 
-                    <li><a>blogit</a></li>
-                    <li><a>some</a></li>
+                    <li><a href="#koti">koti</a></li>
+                    <li><a href="#quiz">quiz</a></li>
+                    <li><a href="#info">info</a></li> 
+                    <li><a href="#blogit">blogit</a></li>
+                    <li><a href="#some">some</a></li>
                 </ul>
                 </nav>
         </section>
         <section id="koti">
-        </section>
-        <section id="quiz">
+          <div className="container">
+            <h2>Sisäänheittoteksti!</h2>
+            <div>
+              <iframe
+                width="853"
+                height="480"
+                src={`https://www.youtube.com/embed/9WGq040-yww`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded youtube"
+              />
+            </div>
+          </div>
         </section>
         <section id="info">
+        </section>
+        <section id="quiz">
         </section>
         <section id="blogit">
           <div> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper nibh eu luctus mattis. Donec tincidunt sollicitudin enim rutrum vulputate. Phasellus quam elit, accumsan nec est et, posuere tempus odio. In ac felis aliquam, bibendum orci non, dapibus augue. Pellentesque bibendum consectetur nisi, ac porta elit tincidunt in. Aliquam nunc massa, eleifend non tortor sit amet, pellentesque bibendum augue. Maecenas vitae sollicitudin ipsum. Pellentesque arcu justo, molestie at justo sit amet, luctus pellentesque ligula.
@@ -34,6 +47,9 @@ function App() {
         </section>
       </div>
       </header>
+      <footer className="footer">
+        <img alt="logo" src="img/kioski.png"></img>
+      </footer>
     </div>
   );
 }
