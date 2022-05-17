6b40dc079ec6ee7df2a6faa8c2b2061711aaf170
Janne Silfverhuth <silfvej@aalto.fi>
2022-05-17 16:33:03 +0300

    quiziin lisätty alkuteksti ja vastauksien selitykset

--- src/Quiz.js
+++ src/Quiz.js
@@ -4,11 +4,23 @@ import './styles/quiz.css';
 export default function Quiz() {
 	const questions = [
 		{
+			questionText: 'Testaa tietosi pikamuodin vaikutuksesta ympäristöömme',
+			answerOptions: [
+				{ answerText: 'Aloita', isCorrect: false },
+			],
+		},
+		{
 			questionText: 'Kuinka paljon vettä kuluu yhden t-paidan valmistamiseen?',
 			answerOptions: [
 				{ answerText: '350 litraa', isCorrect: false },
 				{ answerText: '1100 litraa', isCorrect: false },
 				{ answerText: '2700 litraa', isCorrect: true },
+			],
+		},
+		{
+			questionText: 'Yhden t-paidan valmistukseen kuluu keskimäärin 2700 litraa vettä. Tämä vastaa yhden henkilön juomavesiä 2,5 vuoden ajalta.',
+			answerOptions: [
+				{ answerText: 'Seuraava', isCorrect: false },
 			],
 		},
 		{
@@ -20,11 +32,23 @@ export default function Quiz() {
 			],
 		},
 		{
+			questionText: 'Nämä sertifikaatit eivät vielä kerro mitään tuotteen vastuullisuudesta. Pikamuotiyrityksellä on usein vaatteilleen omia sertifikaatteja, kuten H&M:n Conscious Collection sekä Lindexin Sustainable Choice. Nämä merkinnät eivät kuitenkaan tarkoita, että vaatekappale olisi valmistettu ekologisesti tai eettisesti. Näillä merkinnöillä varustetut tuotteet ovat lähes samanlaisia kuin muutkin, mutta esimerkiksi raaka-aineet voivat olla vähemmän myrkyllisiä kuin muissa kyseisen malliston tuotteissa.',
+			answerOptions: [
+				{ answerText: 'Seuraava', isCorrect: false },
+			],
+		},
+		{
 			questionText: 'Mikä on maailman saastuttavin teollisuudenala?',
 			answerOptions: [
 				{ answerText: 'Öljyteollisuus', isCorrect: true },
 				{ answerText: 'Tekstiili- ja muotiteollisuus', isCorrect: false },
 				{ answerText: 'Elintarviketeollisuus', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Öljyteollisuus on kaikkein saastuttavin yksittäinen teollisuudenala. Tekstiili- ja muotiteollisuus on kuitenkin maailman toiseksi saastuttavin teollisuudenala tuottaen enemmän päästöjä kuin lento- ja laivaliikenne yhteensä.',
+			answerOptions: [
+				{ answerText: 'Seuraava', isCorrect: false },
 			],
 		},
 		{
@@ -35,12 +59,24 @@ export default function Quiz() {
 				{ answerText: '70-100 miljoonaa kiloa', isCorrect: true },
 			],
 		},
+		{
+			questionText: 'Suomessa heitetään vuosittain pois noin 70-100 miljoonaa kiloa vaatteita. Suuri osa näistä vaatteista päätyy sekajätteen mukana poltettavaksi.',
+			answerOptions: [
+				{ answerText: 'Seuraava', isCorrect: false },
+			],
+		},
         {
 			questionText: 'Kuinka paljon voit vähentää päästöjä käyttämällä vaatteitasi tuplasti pidempään?',
 			answerOptions: [
 				{ answerText: '14%', isCorrect: false },
 				{ answerText: '44%', isCorrect: true },
 				{ answerText: '94%', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Kasvattamalla vaatteiden käyttöikää kaksinkertaiseksi voit vähentää päästöjä 44% prosentilla. Parhaita keinoja ekologisempaan kuluttamiseen on uusien vaatteiden ostamisen vähentäminen ja kierrätettyjen vaatteiden suosiminen.',
+			answerOptions: [
+				{ answerText: 'Seuraava', isCorrect: false },
 			],
 		},
 	];
@@ -55,24 +91,27 @@ export default function Quiz() {
 		}
 
 		const nextQuestion = currentQuestion + 1;
+
+
+
 		if (nextQuestion < questions.length) {
 			setCurrentQuestion(nextQuestion);
 		} else {
 			setShowScore(true);
 		}
 	};
+
+	let questionOrAnswer = currentQuestion % 2 == 1
+
 	return (
 		<div className='app'>
 			{showScore ? (
 				<div className='score-section'>
-					Sait {score} pistettä, kun maksimi oli {questions.length}
+					Sait {score} pistettä, kun maksimi oli 5
 				</div>
 			) : (
-				<>
-					<div className='question-section'>
-						<div className='question-count'>
-							<span>Kysymys {currentQuestion + 1}</span>/{questions.length}
-						</div>
+				<><div className='question-section'>
+						{questionOrAnswer ? <div className='question-count'> Kysymys {currentQuestion/ 2 + 0.5}/5 </div> : ""}
 						<div className='question-text'>{questions[currentQuestion].questionText}</div>
 					</div>
 					<div className='answer-section'>
