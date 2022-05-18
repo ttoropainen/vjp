ed04bc36535f5c5fa82c62d72632ec045345a94b
Tuomas Toropainen <tuomastoropainen@Tuomass-MacBook-Pro.local>
2022-05-18 16:10:59 +0300

    kyssärit siirretty toiseen filuun

---
+++ src/Questions.js
@@ -0,0 +1,80 @@
+const questions = [
+    {
+        questionText: 'Testaa tietosi pikamuodin vaikutuksesta ympäristöömme',
+        answerOptions: [
+            { answerText: 'Aloita', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Kuinka paljon vettä kuluu yhden t-paidan valmistamiseen?',
+        answerOptions: [
+            { answerText: '350 litraa', isCorrect: false },
+            { answerText: '1100 litraa', isCorrect: false },
+            { answerText: '2700 litraa', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'Yhden t-paidan valmistukseen kuluu keskimäärin 2700 litraa vettä. Tämä vastaa yhden henkilön juomavesiä 2,5 vuoden ajalta.',
+        answerOptions: [
+            { answerText: 'Seuraava', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Mitä vaatteen lapussa lukeva "Conscious Collection" tai "Sustainable Choice" tarkoittaa?',
+        answerOptions: [
+            { answerText: 'Tuote on jollakin tavalla hieman vähemmän haitallinen kuin saman ketjun muut tuotteet', isCorrect: true },
+            { answerText: 'Vaatteen valmistamiseen on käytetty kierrätettyjä materiaaleja', isCorrect: false },
+            { answerText: 'Työntekijöille on maksettu tuotteen valmistamisesta kohtuullista palkkaa.', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Nämä sertifikaatit eivät vielä kerro mitään tuotteen vastuullisuudesta. Pikamuotiyrityksellä on usein vaatteilleen omia sertifikaatteja, kuten H&M:n Conscious Collection sekä Lindexin Sustainable Choice. Nämä merkinnät eivät kuitenkaan tarkoita, että vaatekappale olisi valmistettu ekologisesti tai eettisesti. Näillä merkinnöillä varustetut tuotteet ovat lähes samanlaisia kuin muutkin, mutta esimerkiksi raaka-aineet voivat olla vähemmän myrkyllisiä kuin muissa kyseisen malliston tuotteissa.',
+        answerOptions: [
+            { answerText: 'Seuraava', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Mikä on maailman saastuttavin teollisuudenala?',
+        answerOptions: [
+            { answerText: 'Öljyteollisuus', isCorrect: true },
+            { answerText: 'Tekstiili- ja muotiteollisuus', isCorrect: false },
+            { answerText: 'Elintarviketeollisuus', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Öljyteollisuus on kaikkein saastuttavin yksittäinen teollisuudenala. Tekstiili- ja muotiteollisuus on kuitenkin maailman toiseksi saastuttavin teollisuudenala tuottaen enemmän päästöjä kuin lento- ja laivaliikenne yhteensä.',
+        answerOptions: [
+            { answerText: 'Seuraava', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Kuinka paljon vaatteita heitetään Suomessa vuosittain pois?',
+        answerOptions: [
+            { answerText: 'alle 10 miljoonaa kiloa', isCorrect: false },
+            { answerText: '30-50 miljoonaa kiloa', isCorrect: false },
+            { answerText: '70-100 miljoonaa kiloa', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'Suomessa heitetään vuosittain pois noin 70-100 miljoonaa kiloa vaatteita. Suuri osa näistä vaatteista päätyy sekajätteen mukana poltettavaksi.',
+        answerOptions: [
+            { answerText: 'Seuraava', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Kuinka paljon voit vähentää päästöjä käyttämällä vaatteitasi tuplasti pidempään?',
+        answerOptions: [
+            { answerText: '14%', isCorrect: false },
+            { answerText: '44%', isCorrect: true },
+            { answerText: '94%', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Kasvattamalla vaatteiden käyttöikää kaksinkertaiseksi voit vähentää päästöjä 44% prosentilla. Parhaita keinoja ekologisempaan kuluttamiseen on uusien vaatteiden ostamisen vähentäminen ja kierrätettyjen vaatteiden suosiminen.',
+        answerOptions: [
+            { answerText: 'Seuraava', isCorrect: false },
+        ],
+    },
+];
+
+export default questions
